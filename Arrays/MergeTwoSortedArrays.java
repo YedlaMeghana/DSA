3fe006b8176b460a55c059c1178279036e881f43
Meghana <yedlameghana01@gmail.com>
2025-05-27 15:02:44 +0530

    Create MergeTwoSortedArrays.java

---
+++ Arrays/MergeTwoSortedArrays.java
@@ -0,0 +1,57 @@
+/* Merge two Sorted Arrays Without Extra Space
+Problem statement: Given two sorted arrays arr1[] and arr2[] of sizes n and m in non-decreasing order. Merge them in sorted order.
+Modify arr1 so that it contains the first N elements and modify arr2 so that it contains the last M elements. */
+
+
+import java.util.*;
+public class MergeTwoSortedArrays {
+
+    public static void merge(long[] arr1, long[] arr2, int n, int m) {
+
+        // Declare 2 pointers:
+        int left = n - 1;
+        int right = 0;
+
+        // Swap the elements until arr1[left] is
+        // smaller than arr2[right]:
+        while (left >= 0 && right < m) {
+            if (arr1[left] > arr2[right]) {
+                long temp = arr1[left];
+                arr1[left] = arr2[right];
+                arr2[right] = temp;
+                left--;
+                right++;
+            } else {
+                break;
+            }
+        }
+
+        // Sort arr1[] and arr2[] individually:
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+    }
+
+    public static void main(String[] args) {
+        long[] arr1 = {1, 4, 8, 10};
+        long[] arr2 = {2, 3, 9};
+        int n = 4, m = 3;
+        merge(arr1, arr2, n, m);
+        System.out.println("The merged arrays are:");
+        System.out.print("arr1[] = ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr1[i] + " ");
+        }
+        System.out.print("\narr2[] = ");
+        for (int i = 0; i < m; i++) {
+            System.out.print(arr2[i] + " ");
+        }
+        System.out.println();
+    }
+}
+
+/* OUPUT:
+The merged arrays are: 
+arr1[] = 1 2 3 4 
+arr2[] = 8 9 10 
+*/
+
