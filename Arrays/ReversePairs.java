9706a0f274531b0679c8843a3d368fbc603e1443
Meghana <yedlameghana01@gmail.com>
2025-06-05 21:54:38 +0530

    Create ReversePairs.java

---
+++ Arrays/ReversePairs.java
@@ -0,0 +1,73 @@
+/* Given an array of numbers, you need to return the count of reverse pairs. Reverse Pairs are those pairs where i<j and arr[i]>2*arr[j].
+0 <= a, b, c, d < n
+a, b, c, and d are distinct.
+arr[a] + arr[b] + arr[c] + arr[d] == target
+
+Example :
+Input Format: arr[] = [1,0,-1,0,-2,2], target = 0
+Result: [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+Explanation: We have to find unique quadruplets from the array such that the sum of those elements is equal to the target sum given that is 0.
+The result obtained is such that the sum of the quadruplets yields 0. */
+
+
+import java.util.*;
+
+public class ReversePairs {
+    public static List<List<Integer>> quadruplets(int[] nums, int target) {
+        int n = nums.length; // size of the array
+        List<List<Integer>> ans = new ArrayList<>();
+
+        // sort the given array:
+        Arrays.sort(nums);
+
+        // calculating the quadruplets:
+        for (int i = 0; i < n; i++) {
+            // avoid the duplicates while moving i:
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            for (int j = i + 1; j < n; j++) {
+                // avoid the duplicates while moving j:
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+
+                // 2 pointers:
+                int k = j + 1;
+                int l = n - 1;
+                while (k < l) {
+                    long sum = nums[i] + nums[j] + nums[k]+ nums[l];
+                    if (sum == target) {
+                        List<Integer> temp = new ArrayList<>();
+                        temp.add(nums[i]);
+                        temp.add(nums[j]);
+                        temp.add(nums[k]);
+                        temp.add(nums[l]);
+                        ans.add(temp);
+                        k++;
+                        l--;
+
+                        // skip the duplicates:
+                        while (k < l && nums[k] == nums[k - 1]) k++;
+                        while (k < l && nums[l] == nums[l + 1]) l--;
+                    } else if (sum < target) k++;
+                    else l--;
+                }
+            }
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {4, 3, 3, 4, 4, 2, 1, 2, 1, 1};
+        int target = 9;
+        List<List<Integer>> ans = fourSum(nums, target);
+        System.out.println("The quadruplets are: ");
+        for (List<Integer> it : ans) {
+            System.out.print("[");
+            for (int ele : it) {
+                System.out.print(ele + " ");
+            }
+            System.out.print("] ");
+        }
+        System.out.println();
+    }
+}
+
