48aa67df39a3bbbd54d28aa85bc6586599c01db7
Meghana <yedlameghana01@gmail.com>
2025-06-19 21:28:08 +0530

    Create 258.AddDigits.java

---
+++ 258.AddDigits.java
@@ -0,0 +1,24 @@
+/* 258. Add Digits
+Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+
+Example :
+Input: num = 38
+Output: 2
+Explanation: The process is
+38 --> 3 + 8 --> 11
+11 --> 1 + 1 --> 2 
+Since 2 has only one digit, return it. */
+
+class Solution {
+    public int addDigits(int num) {
+        while (num >= 10) {
+            int sum = 0;
+            while (num != 0) {
+                sum += num % 10;
+                num = num / 10;
+            }
+            num = sum;
+        }
+        return num;
+    }
+}
