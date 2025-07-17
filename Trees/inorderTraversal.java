f81e3eb8a875ae4eb97da2870f99c6d8d9e41105
Meghana <yedlameghana01@gmail.com>
2025-07-17 21:39:38 +0530

    Create inorderTraversal.java

---
+++ Trees/inorderTraversal.java
@@ -0,0 +1,56 @@
+/* INORDER TRAVERSAL
+Complete the  function in your editor below, which has  parameter: a pointer to the root of a binary tree. 
+It must print the values in the tree's inorder traversal as a single line of space-separated values.
+Input Format
+Our hidden tester code passes the root node of a binary tree to your $inOrder* function.
+Output Format
+Print the tree's inorder traversal as a single line of space-separated values. */
+
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    static class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int val){
+            this.val = val;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        TreeNode root = null;
+        for(int i = 0; i < n; i++){
+            int val = sc.nextInt();
+            root = insert(root, val);
+        }
+        postOrder(root); 
+    }
+    
+    public static void postOrder(TreeNode root){
+        if(root == null){
+            return;
+        }
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(root.val + " ");
+    }
+    
+    public static TreeNode insert(TreeNode root, int val){
+        if(root == null){
+            return new TreeNode(val);
+        }
+        if(val <= root.val){
+            root.left = insert(root.left, val);
+        } else {
+            root.right = insert(root.right, val);
+        }
+        return root;
+    }
+}
